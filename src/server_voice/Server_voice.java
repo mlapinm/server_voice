b415a32424e5383cc58fffb4c869f49c2f811e8e
mlapinm <mlapin@rambler.ru>
2019-08-20 19:12:25 +0300

    server_end

--- src/server_voice/Server_voice.java
+++ src/server_voice/Server_voice.java
@@ -11,11 +11,11 @@ package server_voice;
  */
 public class Server_voice {
 
-    /**
-     * @param args the command line arguments
-     */
+    public static boolean calling = false;
     public static void main(String[] args) {
-        // TODO code application logic here
+        server_fr fr = new server_fr();
+        fr.setVisible(true);
+        
     }
     
 }
